3b5c5e60cf545fa39c163e24474d14377d2a89ed
priscilafraifer <priscilafraifer@gmail.com>
2022-03-11 21:44:47 -0300

    1 file modified, 1 added

--- src/app/list-details/list-details.component.ts
+++ src/app/list-details/list-details.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-list-details',
   templateUrl: './list-details.component.html',
-  styleUrls: ['./list-details.component.css']
+  styleUrls: ['./list-details.component.css'],
 })
 export class ListDetailsComponent implements OnInit {
+  item;
 
-  constructor() { }
+  constructor(private router: ActivatedRoute) {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
