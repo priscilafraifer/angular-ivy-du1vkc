1b3b02c27973cff4d7375b1d60060bc2ec77958c
priscilafraifer <priscilafraifer@gmail.com>
2022-03-11 23:00:10 -0300

    1 file modified

--- src/app/list-details/list.ts
+++ src/app/list-details/list.ts
@@ -5,26 +5,32 @@ export const listData = [
     dia: 'Segunda-Feira',
     hora: '09:30-13:00',
     dados: '-',
-    ementa: '-',
-    objetivo: '-',
+    ementa:
+      'PHP histórico e emprego. Instalação e configuração básica do PHP e um IDE. Sintaxe básica do PHP. Usando o PHP como um mecanismo de modelo simples. Panorama das melhores práticas com PHP. Conceitos de programação HTTP. Codificação de caracteres. Localidades, fusos horários e funções de tempo. Strings. Uso de Array e funções de matriz. Orientação a objetos em PHP (Classes, objetos, herança, encapsulamento, polimorfismo, agregação, composição e métodos). Tratamento de exceções de erro. Arquitetura do lado do servidor. Manipulação de dados postados. Enviando e-mail. Sessões e autenticação. Cookies. Arquivo manuseio e armazenamento de dados em arquivos de texto. Gerenciador de banco de dados e suas funções. Frameworks. Web Services, API, RSS, JSON e Ajax. Hospedagem compartilhada',
+    objetivo:
+      'Esta disciplina fornecerá uma visão geral da linguagem script PHP com associado a um gerenciador de banco de dados que utilize a linguagem SQL e como usar essas tecnologias para gerar sites dinâmicos. Introduzir práticas de codificação seguras. Objetivos específicos.  Os estudantes deverão ser capazes de desenvolver um CMS simples ou um aplicativo Web completo (lado cliente e lado servidor',
   },
   {
     id: 2,
     text: 'Desenvolvimento para Servidores II',
     dia: '	Quinta-Feira',
     hora: '	19:00-22:30',
-    dados: '-',
-    ementa: '-',
-    objetivo: '-',
+    dados: 'IDS002A-CIRO CIRNE TRINDADE',
+    ementa:
+      'Conceitos e evolução das tecnologias de programação de servidores. Recursos da linguagem escolhida para servidores na Internet. Padrões de projetos. Integração com sistemas (Google Maps API, Twitter, entre outros',
+    objetivo:
+      'Desenvolver um site completo de e-commerce ou outro tipo de negócio na Internet usando uma linguagem apropriada a servidores, banco de dados e padrões de projeto. Objetivos específicos. Implementar softwares do lado servidor e com uso de uma linguagem de programação e de padrões de projetos mais usuais como MVC, DAO, Composite, Singleton, entre outros',
   },
   {
     id: 3,
     text: 'Padrões de Projeto de Sítios Internet II',
     dia: 'Quarta-Feira',
     hora: '09:30-13:00',
-    dados: '-',
-    ementa: '-',
-    objetivo: '-',
+    dados: 'ISW006A-ALEXANDRE SOBRINO GANANÇA',
+    ementa:
+      'Revisão dos padrões de projeto para Web. Tópicos CSS. Esquemas de posicionamento. Elementos flutuantes. Formulários HTML. Elementos de HTML avançado. Técnicas avançadas de reposição  de imagem. Seletores avançados do CSS. Microformatos. Técnicas de mosaico de imagens. Resolução de Problemas de layout com uso de depuradores apropriados. Design responsivo. Web semântica. Acessibilidade. Internacionalização',
+    objetivo:
+      'Aprendizado das técnicas avançadas de HTML e CSS para criação de páginas Web de layout sofisticado em conformidade com as diretrizes do  W3C. Utilização de microformatos para criar conteúdo semântico e portável. Fornecer ao estudante uma base para criação de sítios acessíveis com aplicação de conceitos do Design Universal para acomodar pessoas com deficiência visual, auditiva, fala, controle motor e distúrbios cognitivos. Objetivos específicos. Reconhecer e aplicar as regras de estilo CSS para tornar formulários HTML mais atrativos e usáveis. Apresentar exemplos de casos concretos de como os padrões de projeto Web beneficiam negócios e usuários. Empregar esquemas de posicionamento para documentos flutuantes e técnicas de substituição de imagens para apresentar elementos em design único com acessibilidade, dispositivos de busca (search engines) amigáveis e textos alternativos. Descrever e diagramar o relacionamento entre formulário  HTML e tecnologias do lado servidor. Criar formulários HTML. Conectar um formulário HTML com um servidor para processamento. Empregar microformatos semânticos para marcar contatos e eventos (hCard) e (hCalendar) e ferramenta de tradução (como http://technorati.com/contacts/) com objetivo de criar links que permitam ao usuário baixar arquivos ou mover conteúdos, traduzir hCard para vCard e incorporá-lo dentro de um programa de agenda de endereços. Empregar CSS para atribuir estilo a conteúdos de microformatos. Empregar propriedades de background com mosaico de imagens e técnicas de mapa de mosaico para criar colunas falsas dando ilusão de sombras e profundidade. Incorporar às páginas HTML acessibilidade que atendam ao nível de prioridade 1 da Web Content Accessibility Guidelines (WCAG) 2.0. Identificar técnicas necessárias à apresentação de conteúdos em formatos internacionais',
   },
   {
     id: 4,
