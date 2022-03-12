442be201fe20ed007cc634b34289576d2c609a93
priscilafraifer <priscilafraifer@gmail.com>
2022-03-11 22:15:11 -0300

    3 files modified

--- src/app/list-details/list.ts
+++ src/app/list-details/list.ts
@@ -1,5 +1,32 @@
 export const listData = [
-  { id: 1, text: 'Texto1' },
-  { id: 2, text: 'Texto2' },
-  { id: 3, text: 'Texto3' },
+  {
+    id: 1,
+    text: 'Desenvolvimento para Servidores I',
+    dia: 'Segunda-Feira',
+    hora: '09:30-13:00',
+  },
+  {
+    id: 2,
+    text: 'Desenvolvimento para Servidores II',
+    dia: '	Quinta-Feira',
+    hora: '	19:00-22:30',
+  },
+  {
+    id: 3,
+    text: 'Padrões de Projeto de Sítios Internet II',
+    dia: 'Quarta-Feira',
+    hora: '09:30-13:00',
+  },
+  {
+    id: 4,
+    text: 'Projeto de Navegação e Interação',
+    dia: '	Quinta-Feira',
+    hora: '09:30-13:00',
+  },
+  {
+    id: 5,
+    text: 'Tópicos Especiais em Sistemas para Internet II',
+    dia: 'Sexta-Feira1',
+    hora: '19:00-22:30',
+  },
 ];
